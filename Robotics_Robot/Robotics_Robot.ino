0ad02b8b44fd4c08a03047fac783c232ead23164
Brenbrit <brendanguillory@gmail.com>
2019-01-09 20:25:00 -0600

    Sensor and Library push
    Got the sensor to work (finally!) and moved the libraries to a "libraries" folder in the root of the repo

--- Robotics_Robot/Robotics_Robot.ino
+++ Robotics_Robot/Robotics_Robot.ino
@@ -1,3 +1,7 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 #include <SparkFun_TB6612.h>
 
 
@@ -23,6 +27,9 @@ Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 //the passenger side motor
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
+//for the blue sensor chip
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 //for measuring the distance. I'll just stick to 2 pins right now because I don't need anymore at the moment
 //lol was he dumb. I don't need anymore right now either tho. Bet I'll eat my words in a few hours
@@ -41,35 +48,55 @@ bool permaStop = false;
 //if this ever gets to be 3, the bot will stop.
 int stopCounter = 0;
 
+//this is used for the sensor - only output once every 10 times
+int bnoCounter = 0;
 
+//we don't want the robot to start as soon as you plug it in
 int startPin = 4;
-
+bool delayStart = true;
 
 
 void setup() {
   Serial.begin(9600);
+  digitalWrite(13, LOW);
   pinMode(startPin, INPUT);
+
   
-  while (digitalRead(startPin) == HIGH) {
-    //do nothing bc start pin is still high.
-    delay(500);
-  }
-  delay(1000);
+  //try to begin the bno sensor
+  Serial.println("beginning the bno");
+  bno.begin();
+  Serial.println("bno began. setting crystal now");
+  bno.setExtCrystalUse(true);
+  Serial.println("bno beginning completed.");
 
   Serial.println("Finished setup");
-
-  motor1.drive(255);
-  motor2.drive(255);
 }
+
+
 
 void loop() {
 
   if (permaStop) {
     motor1.brake();
     motor2.brake();
-    delay(1000);
-    return;
+  } else {
+    if (!delayStart) {
+      //the start pin has been released. we're free!
+      motor1.drive(255);
+      motor2.drive(255);
+    } else {
+      //the start pin is still in. Let's check
+      if (digitalRead(startPin) == HIGH) {
+        //yep, it's still in there.
+        Serial.println("Waiting for pin 4");
+      } else {
+        delayStart = false;
+      }
+    }
   }
+
+  //for the sensor
+  //bno.setExtCrystalUse(true);
   
   //set ultra to trig mode, write LOW
   pinMode(ultraPin, OUTPUT);
@@ -92,7 +119,8 @@ void loop() {
   if (distance <= maxDistance && distance >= minDistance) {
     
     //output the distance to the serial port. Maybe comment out for application
-    Serial.println(distance);
+    Serial.print(distance);
+    Serial.println("cm");
 
     //for the stop part
     if (distance <= 20) {
@@ -103,6 +131,33 @@ void loop() {
       }
     }
     
+  }
+
+  sensors_event_t event;
+  bno.getEvent(&event);
+  
+  if (bnoCounter == 0) {
+    //output the sensor data for the car's orientation every 10th time
+    Serial.print("X");
+    Serial.print(event.orientation.x, 4);
+    Serial.print("\tY");
+    Serial.print(event.orientation.y, 4);
+    Serial.print("\tZ");
+    Serial.println(event.orientation.z, 4);
+    bnoCounter++;
+  } else {
+    if (bnoCounter == 10) {
+      bnoCounter = 0;
+    } else {
+      bnoCounter++;
+    }
+  }
+
+  if (event.orientation.z >= 40) {
+    permaStop = true;
+  } 
+  if (event.orientation.y >= 40) {
+    permaStop = true;
   }
 
   //if we don't delay this loop, the sensor will constantly output 7 or 8 cm. do not do not remove.
